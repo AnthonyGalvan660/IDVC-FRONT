3b78d8fd30c06ee8590054738d97747da3dfdb18
AnthonyGalvan660 <anthonygalvan660@gmail.com>
2025-07-27 01:41:01 -0500

    Service render

--- src/app/servicios/validacion.service.ts
+++ src/app/servicios/validacion.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ValidacionService {
-  private apiUrl = 'http://localhost:3000'; // Puerto donde corre tu backend Node.js
+  private apiUrl = 'https://idvc-back.onrender.com'; // Puerto donde corre tu backend Node.js
 
   constructor(private http: HttpClient) {}
 
