c86e531e2b3d6b7cb9586aedfbe416bb3aba25e4
AnthonyGalvan660 <anthonygalvan660@gmail.com>
2025-07-27 17:00:02 -0500

    Actualizado FRONT VERCEL

--- src/app/servicios/validacion.service.ts
+++ src/app/servicios/validacion.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ValidacionService {
-  private apiUrl = 'http://localhost:3000'; // Local backend
+  private apiUrl = 'https://idvc-back.onrender.com'; // Local backend
 
   constructor(private http: HttpClient) {}
 
